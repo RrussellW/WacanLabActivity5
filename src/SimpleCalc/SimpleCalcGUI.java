21ce25ae5c1bb52029241a1dfbac114a979775dc
Renz Russell T. Wacan <rrwacan@gmail.com>
2022-12-17 02:52:29 +0800

    Complete SimpleCalc

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -3,6 +3,8 @@ package SimpleCalc;
 import LeapYear.LeapYearGUI;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SimpleCalcGUI extends JFrame{
     private JPanel panel1;
@@ -12,10 +14,47 @@ public class SimpleCalcGUI extends JFrame{
     private JTextField tfNumber2;
     private JTextField lblResult;
 
+    public SimpleCalcGUI () {
+
+        lblResult.setEditable(false);
+        btnCompute.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                double num1 = Double.parseDouble(tfNumber1.getText());
+
+                double num2 = Double.parseDouble(tfNumber2.getText());
+
+                String operator = (String)cbOperations.getSelectedItem();
+
+                double result=0;
+
+                switch (operator) {
+                    case "+":
+                        result = num1 + num2;
+                        break;
+                    case "-":
+                        result = num1 - num2;
+                        break;
+                    case "*":
+                        result = num1 * num2;
+                        break;
+                    case "/":
+                        result = num1 / num2;
+                        break;
+                }
+
+                lblResult.setText(String.format("%.2f",result));
+
+            }
+        });
+    }
+
     public static void main(String[] args) {
         SimpleCalcGUI app = new SimpleCalcGUI();
         app.setContentPane(app.panel1);
-        app.setSize(550,300);
+        app.setSize(600,300);
         app.setTitle("Simple Calculator");
         app.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         app.setVisible(true);
