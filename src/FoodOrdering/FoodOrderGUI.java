f1b966768f5696d226b262ea9a7ca18daec7d151
Renz Russell T. Wacan <rrwacan@gmail.com>
2022-12-17 04:04:01 +0800

    Complete FoodOrdering

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -1,6 +1,8 @@
 package FoodOrdering;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class FoodOrderGUI extends JFrame{
     private JPanel panel1;
@@ -15,6 +17,53 @@ public class FoodOrderGUI extends JFrame{
     private JRadioButton rb5;
     private JRadioButton rb10;
     private JRadioButton rb15;
+
+    public FoodOrderGUI () {
+
+
+        btnOrder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                double total = 0.0;
+
+                double disc = 0.0;
+
+                    if (cPizza.isSelected()) {
+                        total += 100.0;
+                    }
+                    if (cBurger.isSelected()) {
+                        total += 80.0;
+                    }
+                    if (cFries.isSelected()) {
+                        total += 65.0;
+                    }
+                    if (cSoftDrinks.isSelected()) {
+                        total += 55.0;
+                    }
+                    if (cTea.isSelected()) {
+                        total += 50.0;
+                    }
+                    if (cSundae.isSelected()) {
+                        total += 40.0;
+                    }
+
+                    if (rb5.isSelected()) {
+                        disc = 0.05;
+                    } else if (rb10.isSelected()) {
+                        disc = 0.1;
+                    } else if (rb15.isSelected()) {
+                        disc = 0.15;
+                    } else if (rbNone.isSelected()) {
+                        disc = 0.0;
+                    }
+
+                    double cost = total - (total * disc);
+
+                    JOptionPane.showMessageDialog(null, "The total price is Php " + String.format("%.2f", cost));
+            }
+        });
+    }
 
     public static void main(String[] args) {
         FoodOrderGUI app = new FoodOrderGUI();
